a97c33d16d774b4caf98cdbeadbadd056d89637d
Francisco Oliveira <fr.pdoliv@gmail.com>
2022-02-17 09:55:30 +0000

    Finished Author Controller

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Problems\NotFoundProblem;
 use App\Models\Author;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthorController extends Controller
 {
+    private static int $DEFAULT_N_PAGE_RECORDS = 100;
+
     /**
      * Display a listing of the resource.
      *
@@ -15,17 +19,30 @@ class AuthorController extends Controller
      */
     public function index(Request $request)
     {
+        $pageSize = $request->query('n_page_records', self::$DEFAULT_N_PAGE_RECORDS);
+        return Auth::user()->authors()->paginate($pageSize);
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'name' => ['required'],
+            'birthdate' => ['date', 'before:today', 'nullable'],
+            'birthplace' => ['max:255', 'nullable'],
+        ]);
+
+        Author::create([
+            ...$validatedData,
+            'user_id' => Auth::id()
+        ]);
+
+        return response()->json('', 204);
     }
 
     /**
@@ -36,7 +53,13 @@ class AuthorController extends Controller
      */
     public function show(Author $author)
     {
-        return response()->json($author);
+        if ($author->user_id === Auth::id()) {
+            return response()->json($author);
+        }
+        return response()->json(
+            (new NotFoundProblem(request(), __('Author error')))->toArray(),
+            NotFoundProblem::$status
+        );
     }
 
     /**
@@ -44,21 +67,48 @@ class AuthorController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, Author $author)
     {
-        //
+        if ($author->user_id === Auth::id()) {
+            $validatedData = $request->validate([
+                'name' => ['nullable'],
+                'birthdate' => ['date', 'before:today', 'nullable'],
+                'birthplace' => ['max:255', 'nullable'],
+            ]);
+
+            Author::updateOrCreate(
+                $author->toArray(),
+                $validatedData
+            );
+
+            return response()->json('', 204);
+        }
+
+        return response()->json(
+            (new NotFoundProblem(request(), __('Author error')))->toArray(),
+            NotFoundProblem::$status
+        );
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Author  $author
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Author $author)
     {
-        //
+        if ($author->user_id === Auth::id()) {
+            $author->delete();
+
+            return response()->json('', 204);
+        }
+
+        return response()->json(
+            (new NotFoundProblem(request(), __('Author error')))->toArray(),
+            NotFoundProblem::$status
+        );
     }
 }
