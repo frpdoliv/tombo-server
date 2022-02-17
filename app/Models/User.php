884c17f25304a58f5773754ca8ebef6edca51da8
Francisco Oliveira <fr.pdoliv@gmail.com>
2022-02-17 09:55:10 +0000

    Created relation methods in User

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+
+    public function locations()
+    {
+        return $this->hasMany(Location::class);
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function authors()
+    {
+        return $this->hasMany(Author::class);
+    }
 }
